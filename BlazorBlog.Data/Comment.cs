af69ac71bd7178eaad2d5212717c6d7a0422f478
boilerb83 <boilerb83@gmail.com>
2020-10-19 15:16:34 -0400

    multiple classes

---
+++ BlazorBlog.Data/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Data
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public User Author { get; set; }
+        public Post CommentPost { get; set; }
+    }
+}
