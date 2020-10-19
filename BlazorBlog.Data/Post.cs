80c0397a237a71aebd0ee7703ad0a637d36e7b3a
Robert Darku <rdarku@gmail.com>
2020-10-19 16:50:13 -0400

    Updated Data models

--- BlazorBlog.Data/Post.cs
+++ BlazorBlog.Data/Post.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorBlog.Data
 {
     public class Post
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public User Author { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+
+        public virtual User Author { get; set; }
     }
 }
--- BlazorBlog.Data/User.cs
+++ BlazorBlog.Data/User.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorBlog.Data
 {
     public class User
     {
+        [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
